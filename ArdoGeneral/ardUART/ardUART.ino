1808b59b637feb3fa6065f2d6cc19b1ea65d5406
omer& <wowhui@wow.net>
2025-05-06 13:53:42 +0300

    shit u all

---
+++ ArdoGeneral/ardUART/ardUART.ino
@@ -0,0 +1,86 @@
+#include <SoftwareSerial.h>
+#include <Adafruit_NeoPixel.h>
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN  3
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS  24
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 50; // delay for half a second
+
+// Define RX and TX pins
+const int rxPin = 9;
+const int txPin = 10;
+
+// Create a SoftwareSerial object
+SoftwareSerial mySerial(rxPin, txPin);
+
+void setup() {
+  strip.begin(); // This initializes the NeoPixel library.
+  strip.clear();
+  strip.show();   // make sure it is visible
+  int red = 255;
+  int gre = 0;
+  int blue = 0;
+  for(int i = 0; i < NUMPIXELS; i++){
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    strip.setPixelColor(i, gre, red, blue);
+    strip.show(); 
+    // This sends the updated pixel color to the hardware.
+    delay(delayval); 
+    // Delay for a period of time (in milliseconds).
+  }
+  // Start hardware serial communication for debugging
+  Serial.begin(9600);
+  while (!Serial);
+
+  // Start software serial communication
+  mySerial.begin(9600);
+  Serial.println("Full-Duplex UART Communication Initialized.");
+}
+
+void loop() {
+  // Check if data is available to read from the software serial
+  if (mySerial.available()) {
+    
+    char incomingByte = mySerial.read();
+    
+    if (incomingByte == '1'){
+      
+      int red = 0;
+      int gre = 255;
+      int blue = 0;
+      for(int i = 0; i < NUMPIXELS; i++){
+        // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+        strip.setPixelColor(i, gre, red, blue);
+        strip.show(); 
+        // This sends the updated pixel color to the hardware.
+        delay(delayval); 
+        // Delay for a period of time (in milliseconds).
+      }
+      
+      delay(2000);
+      
+      red = 255;
+      gre = 0;
+      blue = 0;
+      for(int i = 0; i < NUMPIXELS; i++){
+        // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+        strip.setPixelColor(i, gre, red, blue);
+        strip.show(); 
+        // This sends the updated pixel color to the hardware.
+        delay(delayval); 
+        // Delay for a period of time (in milliseconds).
+      }
+      
+    }
+    Serial.print(incomingByte);
+  }
+
+  // Check if data is available to read from the hardware serial
+  if (Serial.available()) {
+    char outgoingByte = Serial.read();
+    mySerial.print(outgoingByte);
+  } 
+}
---
+++ ArdoGeneral/neopixels_simple_code/neopixels_simple_code.ino
@@ -0,0 +1,33 @@
+#include <Adafruit_NeoPixel.h>
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN  6
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS  12
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+
+void setup() {
+  strip.begin(); // This initializes the NeoPixel library.
+  strip.clear();
+  strip.show();   // make sure it is visible
+
+}
+
+void loop() {
+
+  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+
+  int red = random(0, 255);
+  int gre = random(0, 255);
+  int blue = random(0, 255);
+  for(int i = 0; i < NUMPIXELS; i++){
+    // pixels.Color takes GRB values, from 0,0,0 up to 255,255,255 [in the order GRB]
+    strip.setPixelColor(i, gre, red, blue);
+    strip.show(); 
+    // This sends the updated pixel color to the hardware.
+    delay(delayval); 
+    // Delay for a period of time (in milliseconds).
+  }
+}
